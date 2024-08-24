9d9daae05cb92fce9918c8d49eb292b14cfb00e7
Dorian Marié <dorian@dorianmarie.com>
2024-08-24 15:06:55 +0200

    add version and help options to yaml-map command

--- spec/yaml_map_spec.rb
+++ spec/yaml_map_spec.rb
@@ -1,6 +1,23 @@
 # frozen_string_literal: true
 
 require "spec_helper"
+require "tempfile"
 
-RSpec.describe "each" do
+RSpec.describe "yaml-map" do
+  it "works" do
+    file = Tempfile.create
+    File.write(file, <<~INPUT)
+      - 1
+      - 2
+      - 3
+    INPUT
+    expect(
+      `cat #{file.path} | bin/yaml-map "it += 1"`
+    ).to eq(<<~OUTPUT)
+      ---
+      - 2
+      - 3
+      - 4
+    OUTPUT
+  end
 end
